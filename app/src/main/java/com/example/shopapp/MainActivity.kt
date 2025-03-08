a08fbafdcd93b27d33184ee2c18e244519a85f65
theqi106 <phkhhulolmb1@gmail.com>
2025-03-08 11:38:19 +0700

    Login screen

--- app/src/main/java/com/example/shopapp/MainActivity.kt
+++ app/src/main/java/com/example/shopapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.shopapp.presentation.LoginScreenUi
 import com.example.shopapp.presentation.SignUpScreen
 import com.example.shopapp.ui.theme.ShopAppTheme
 
@@ -20,7 +21,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ShopAppTheme {
-                SignUpScreen()
+                LoginScreenUi()
             }
         }
     }
